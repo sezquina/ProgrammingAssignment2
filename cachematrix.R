0ce9dee57b9dc5665584e001eac0dfdd1c7cca21
sezquina <sezquina@yahoo.com.br>
2014-06-21 21:02:05 -0300

    This is the final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+#These functions are used to save the square matrix on another environment, 
+#calculate the inverse using solve() and retrieve the inversed matrix 
+#once it was already calculated
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#Saves the matrix on the cache and calculates the inverse, saving the inversed matrix
+makeCacheMatrix <- function(z = matrix()) {
+  t <- NULL
+  set <- function(a) {
+    z <<- a
+    t <<- NULL
+  }
+  get <- function() z
+  setval <- function(solve) t <<- solve
+  getval <- function() t
+  list(set = set, get = get,
+       setval = setval,
+       getval = getval)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+#this function retrieves the inversed matrix from the other environment
+cacheSolve <- function(z, ...) {
         ## Return a matrix that is the inverse of 'x'
+  t <- z$getval()
+  if(!is.null(t)) {
+    message("getting cached data")
+    return(t)
+  }
+  data <- z$get()
+  t <- solve(data, ...)
+  z$setval(t)
+  t
 }
+
